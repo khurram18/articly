7c191c740665331936b968d4a294daa8b3c8afa6
Khurram Shehzad <khurram.pak522@gmail.com>
2020-02-20 13:21:29 +0500

    Implement article detail screen

--- Articly/ViewControllers/ArticleDetail/ArticleDetailViewController.swift
+++ Articly/ViewControllers/ArticleDetail/ArticleDetailViewController.swift
@@ -6,12 +6,17 @@
 //  Copyright © 2020 Example. All rights reserved.
 //
 
+import AlamofireImage
 import UIKit
 
 class ArticleDetailViewController: UIViewController {
 
 static let storyboardID = "articleDetailViewControllerStoryboardID"
   
+@IBOutlet weak var detailTextView: UITextView!
+@IBOutlet weak var titleLabel: UILabel!
+@IBOutlet weak var dateLabel: UILabel!
+@IBOutlet weak var imageView: UIImageView!
 var viewModel: ArticleViewModel?
   
 override func viewDidLoad() {
@@ -19,8 +24,22 @@ override func viewDidLoad() {
   updateView()
 }
   
-private func updateView() {
+@IBAction func onOpenInBrowserTap(_ sender: Any) {
+  guard let urlString = viewModel?.url,
+    let url = URL(string: urlString),
+    UIApplication.shared.canOpenURL(url) else { return }
+  UIApplication.shared.open(url, options: [: ], completionHandler: nil)
+}
   
+private func updateView() {
+  guard let viewModel = viewModel else { return }
+  
+  if let urlString = viewModel.imageUrl, let url = URL(string: urlString) {
+    imageView.af_setImage(withURL: url, filter: AspectScaledToFillSizeFilter(size: imageView.bounds.size), imageTransition: UIImageView.ImageTransition.crossDissolve(0.5), runImageTransitionIfCached: false)
+  }
+  dateLabel.text = viewModel.date
+  titleLabel.text = viewModel.heading
+  detailTextView.text = viewModel.detailText
 }
   
 }
--- Articly/ViewModels/ArticleViewModel.swift
+++ Articly/ViewModels/ArticleViewModel.swift
@@ -11,9 +11,31 @@ import Foundation
 final class ArticleViewModel {
   
 private let article: Article
+private var dateFormatter: DateFormatter = {
+  let formatter = DateFormatter()
+  formatter.dateStyle = .long
+  formatter.timeStyle = .short
+  return formatter
+}()
   
 var heading: String {
   article.abstract
+}
+  
+var date: String {
+  dateFormatter.string(from: article.publishedDate)
+}
+  
+var imageUrl: String? {
+  article.largeImage
+}
+  
+var detailText: String {
+  article.leadParagraph
+}
+  
+var url: String {
+  article.webUrl
 }
   
 init(article: Article) {
